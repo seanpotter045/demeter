8e90fd48ceecdcf158ac5aad4464afab63577998
seanpotter045 <seanpotter045@gmail.com>
2025-02-05 19:46:43 -0500

    Got the frontend running

--- demeter-frontend/app.js
+++
@@ -1,4 +0,0 @@
-import React, { createContent, useState, useEffect } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
-
-import LandingPage from "./components/LandingPage";
